4e1811fbd27656d4d755f38221b25c8dcbc3eae8
ahmadasadullah04 <ahmadassadullah28@gmail.com>
2025-12-01 23:48:51 +0700

    Create index.php

---
+++ pratikum 4/codingan/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Pendaftaran Sekolah</title>
+</head>
+<body>
+
+<h2>FORM PENDAFTARAN SISWA BARU</h2>
+
+<form action="proses_daftar.php" method="post">
+
+Nama Lengkap : <br>
+<input type="text" name="nama" required><br><br>
+
+NISN : <br>
+<input type="text" name="nisn" required><br><br>
+
+Tempat Lahir : <br>
+<input type="text" name="tempat" required><br><br>
+
+Tanggal Lahir : <br>
+<input type="date" name="tgl" required><br><br>
+
+Jenis Kelamin : <br>
+<select name="jk">
+    <option>Laki-laki</option>
+    <option>Perempuan</option>
+</select><br><br>
+
+Agama : <br>
+<select name="agama">
+    <option>Islam</option>
+    <option>Kristen</option>
+    <option>Hindu</option>
+    <option>Budha</option>
+    <option>Konghucu</option>
+</select><br><br>
+
+Alamat Lengkap : <br>
+<textarea name="alamat" required></textarea><br><br>
+
+Nomor HP : <br>
+<input type="text" name="hp"><br><br>
+
+Email : <br>
+<input type="email" name="email"><br><br>
+
+<button type="submit">Daftar</button>
+
+</form>
+
+</body>
+</html>
