05f6c296e97e46a080e7357476f924418b2c5cee
ahmadasadullah04 <ahmadassadullah28@gmail.com>
2025-12-01 23:52:45 +0700

    Create login.php

---
+++ pratikum 6/codingan/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>LOGIN USER</h2>
+
+<form action="login_proses.php" method="post">
+
+Username :
+<input type="text" name="username" required><br><br>
+
+Password :
+<input type="password" name="password" required><br><br>
+
+<button type="submit">Login</button>
+
+</form>
+
+<a href="register.php">Belum punya akun? Register</a>
+
+</body>
+</html>
